6ada20c62cbc4f819b1a79acc0ab87dfb30f2bb6
Aryan Sagavekar <sagavekararyan@gmail.com>
2023-08-28 22:52:22 +0530

    created movement of the player, next is to add physics
    Signed-off-by: Aryan Sagavekar <sagavekararyan@gmail.com>

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:my_first_game/pixel_adv.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Flame.device.fullScreen();
+  await Flame.device.setLandscape();
+
+  PixelAdventure game = PixelAdventure();
+  runApp(GameWidget(game: kDebugMode ? PixelAdventure() : game));
+}
